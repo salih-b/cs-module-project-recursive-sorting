38d607a24498bd0f5489cc759e9e741350b7175a
salih <salihmorta17@gmail.com>
2020-07-22 19:53:17 -0400

    bug fixed

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -6,47 +6,50 @@ def merge(arrA, arrB):
     # Your code here
     a = 0
     b = 0
-
+    print(f"{merged_arr} <----- nothing done yet")
     while a < len(arrA) and b < len(arrB):
         if arrA[a] < arrB[b]:
             merged_arr.append(arrA[a])
+            merged_arr.pop(0)
             a += 1
         else:
             merged_arr.append(arrB[b])
+            merged_arr.pop(0)
             b += 1
 
             # check both arrays to see if the pointer is still in range of its array
     if a < len(arrA):
         # arrA still has leftover elements 
         # push them all to the merged array 
+        left_over = len(arrA[a:])
         merged_arr.extend(arrA[a:])
+        for a in range(left_over):
+            print(f"just curious : {a}")
+            merged_arr.pop(0)
 
     if b < len(arrB):
+        left_over = len(arrB[b:])
         merged_arr.extend(arrB[b:])
+        for a in range(left_over):
+            print(f"just curious : {a}")
+            merged_arr.pop(0)
 
     return merged_arr
 
-# arrA = [3, 6, 8, 12, 15]
-# arrB = [1, 4, 5, 9, 11]
-# print(merge(arrA, arrB))
+arrA = [3, 6, 8, 12, 15]
+arrB = [1, 4, 5, 9, 11]
+print(merge(arrA, arrB))
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     if len(arr) > 1:
         left = merge_sort(arr[0:len(arr) // 2])
-        right = merge_sort(arr[len(arr) // 2 :])
+        right = merge_sort(arr[len(arr) // 2 : ])
 
         arr = merge(left, right)
 
 
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
-# utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
-# or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+# arr = [45, 12, 89, 14, 67, 45, 23, 90, 11]
+# print(merge_sort(arr))
